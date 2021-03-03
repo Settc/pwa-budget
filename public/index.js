c09f06328f4ea7f9f34bff3151890cc740f4e835
Thomas Whittle <tewhittle@gmail.com>
2021-03-02 20:06:46 -0500

    Add manifest

--- public/index.js
+++ public/index.js
@@ -1,6 +1,18 @@
 let transactions = [];
 let myChart;
 
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function(){
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('Service Worker registration successful with scope ', registration.scope);
+    }, function(err){
+      console.log('Service Worker registration unsuccessful: ', err);
+    });
+  });
+}
+
+
 fetch("/api/transaction")
   .then(response => {
     return response.json();
